016add8c6e6559aa30ddf6e36def82623c7333d5
danx9000 <43157277+danx9000@users.noreply.github.com>
2018-09-10 20:49:07 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_DanielChoe.py
@@ -0,0 +1,17 @@
+# Calculating profit by multiplying it with 23%
+# 9/10/18
+# CTI-110 P2T1 - Sales Prediction
+# Daniel Choe
+
+# Get the projected total sales.
+total_sales=   float(input ('Enter the projected sales: ') )
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * .23
+
+# Display the profit
+print ('The profit is $', format (profit, ',.2f'))
+
+# put a value
+# the value will be multiplied by .23
+# the value will show your profit
